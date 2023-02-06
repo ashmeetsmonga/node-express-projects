39033b528199956889458cebd871bd6da94c56ae
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-06 21:23:42 +0530

    routes setup

--- 01. Task Manager/app.js
+++ 01. Task Manager/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasks = require("./routes/tasks");
+
+const app = express();
+const PORT = 5000;
+
+//middleware
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+	res.send("Hello There");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(PORT, () => console.log("Server started on PORT:", PORT));
