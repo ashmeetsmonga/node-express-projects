bd23dfd7fef0fedafbe044654e75502926259fb6
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-07 13:44:44 +0530

    updateTask api functionality added

--- 01. Task Manager/controllers/tasks.js
+++ 01. Task Manager/controllers/tasks.js
@@ -28,8 +28,17 @@ const createTask = async (req, res) => {
 	}
 };
 
-const updateTask = (req, res) => {
-	res.send("update task");
+const updateTask = async (req, res) => {
+	try {
+		const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		if (!task) return res.status(404).json({ msg: `No task with id: ${req.params.id}` });
+		res.status(200).json(task);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 };
 
 const deleteTask = async (req, res) => {
