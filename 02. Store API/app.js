4e8de4116c8ab80a25e13d21a96c90ad2cfae7be
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-08 15:11:00 +0530

    basic setup

--- 02. Store API/app.js
+++ 02. Store API/app.js
@@ -1,1 +1,11 @@
-console.log("02 Store API");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+//middlewares
+app.use(express.json());
+
+//app.get("/", (req, res) => res.send("Hello"));
+
+app.listen(PORT, () => console.log("Server started on port:", PORT));
