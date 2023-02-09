ee7e28501a92a10f172ebd2d495fe4d6d6044d9a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-09 14:34:17 +0530

    query params search

--- 02. Store API/controllers/products.js
+++ 02. Store API/controllers/products.js
@@ -1,16 +1,20 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({});
+	const products = await Product.find({
+		name: { $regex: "accent", $options: "i" },
+	});
 	return res.status(200).json(products);
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured } = req.query;
+	const { featured, company, name } = req.query;
 
 	const queryObject = {};
 
 	if (featured) queryObject.featured = featured === "true";
+	if (company) queryObject.company = company;
+	if (name) queryObject.name = { $regex: name, $options: "i" };
 
 	console.log(queryObject);
 	const products = await Product.find(queryObject);
