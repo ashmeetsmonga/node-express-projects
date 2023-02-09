92485adaff7245b95e816ab858dd0c826d0ed5bf
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-09 14:53:35 +0530

    query params sort

--- 02. Store API/controllers/products.js
+++ 02. Store API/controllers/products.js
@@ -1,23 +1,28 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({
-		name: { $regex: "accent", $options: "i" },
-	});
+	const products = await Product.find({}).sort("-price");
 	return res.status(200).json(products);
 };
 
 const getAllProducts = async (req, res) => {
-	const { featured, company, name } = req.query;
+	const { featured, company, name, sort } = req.query;
 
 	const queryObject = {};
+	let sortParams;
 
 	if (featured) queryObject.featured = featured === "true";
 	if (company) queryObject.company = company;
 	if (name) queryObject.name = { $regex: name, $options: "i" };
 
-	console.log(queryObject);
-	const products = await Product.find(queryObject);
+	let results = Product.find(queryObject);
+	if (sort) {
+		let sortedList = sort.split(",").join(" ");
+		results = results.sort(sortedList);
+	} else {
+		results = results.sort("createdAt");
+	}
+	const products = await results;
 	return res.status(200).json({ hits: products.length, products });
 };
 
