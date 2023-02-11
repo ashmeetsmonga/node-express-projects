ca012fec0eef8614635fba0e97b38c3270ccc360
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-11 12:15:14 +0530

    token not found handling

--- 03. JWT Basics/controllers/main.js
+++ 03. JWT Basics/controllers/main.js
@@ -15,6 +15,9 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith("Bearer"))
+		throw new CustomAPIError("No token found", 401);
 	const randomNumber = Math.floor(Math.random() * 100);
 	res.status(200).json({ msg: "Hello user", secret: `Your lucky number is ${randomNumber}` });
 };
