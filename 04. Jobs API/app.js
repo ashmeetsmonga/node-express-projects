960be24ba147703805a27214711d5ec2bca55b34
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-12 15:39:13 +0530

    routes added

--- 04. Jobs API/app.js
+++ 04. Jobs API/app.js
@@ -6,10 +6,15 @@ const app = express();
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ 04. Jobs API/controllers/jobs.js
@@ -0,0 +1,21 @@
+const getJob = async (req, res) => {
+	res.send("getJob");
+};
+
+const getAllJobs = async (req, res) => {
+	res.send("getAllJobs");
+};
+
+const createJob = async (req, res) => {
+	res.send("createJob");
+};
+
+const updateJob = async (req, res) => {
+	res.send("updateJob");
+};
+
+const deleteJob = async (req, res) => {
+	res.send("deleteJob");
+};
+
+module.exports = { getJob, getAllJobs, createJob, updateJob, deleteJob };
