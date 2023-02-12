233807eb4be1bdb8918e26f7cce183ef99e5e192
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-12 15:46:30 +0530

    wrong starter files added, corrected

--- 04. Jobs API/middleware/not-found.js
+++ 04. Jobs API/middleware/not-found.js
@@ -1,5 +1,3 @@
-const { StatusCodes } = require("http-status-codes");
+const notFound = (req, res) => res.status(404).send('Route does not exist')
 
-const notFound = (req, res) => res.status(StatusCodes.NOT_FOUND).send("Route does not exist");
-
-module.exports = notFound;
+module.exports = notFound
