ad0ac7eedef75e62ee1e42abed134f8af768ab04
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-13 12:22:27 +0530

    password hashed

--- 04. Jobs API/controllers/auth.js
+++ 04. Jobs API/controllers/auth.js
@@ -1,13 +1,18 @@
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
 	const { username, email, password } = req.body;
-	if (!username || !email || !password)
+	if (!username || !email || !password) {
 		throw new BadRequestError("Please provide username, email and password");
+	}
 
-	const newUser = await User.create({ ...req.body });
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const newUser = await User.create({ ...req.body, password: hashedPassword });
 	res.status(StatusCodes.CREATED).json(newUser);
 };
 
