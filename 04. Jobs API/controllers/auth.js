b266154b942bfd85ccfb0e62848ad1ec7b899ab1
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-13 12:14:51 +0530

    added validation in controller for register

--- 04. Jobs API/controllers/auth.js
+++ 04. Jobs API/controllers/auth.js
@@ -1,9 +1,13 @@
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
 const User = require("../models/User");
 
 const register = async (req, res) => {
-	console.log(req.body);
-	const newUser = await User.create(req.body);
+	const { username, email, password } = req.body;
+	if (!username || !email || !password)
+		throw new BadRequestError("Please provide username, email and password");
+
+	const newUser = await User.create({ ...req.body });
 	res.status(StatusCodes.CREATED).json(newUser);
 };
 
